8ab4c41aa3d3dea40a6d55d0dd9019afc58e5aa9
Han Wang <erenwh@gmail.com>
2017-08-24 14:35:29 -0400

    init commit

---
+++ cmake-build-debug/CMakeFiles/cs252.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/hanwang/cs252/lab1-src/MyMalloc.c" "/home/hanwang/cs252/cmake-build-debug/CMakeFiles/cs252.dir/lab1-src/MyMalloc.c.o"
+  "/home/hanwang/cs252/lab1-src/test0.c" "/home/hanwang/cs252/cmake-build-debug/CMakeFiles/cs252.dir/lab1-src/test0.c.o"
+  "/home/hanwang/cs252/lab1-src/test1-1.c" "/home/hanwang/cs252/cmake-build-debug/CMakeFiles/cs252.dir/lab1-src/test1-1.c.o"
+  "/home/hanwang/cs252/lab1-src/test1-2.c" "/home/hanwang/cs252/cmake-build-debug/CMakeFiles/cs252.dir/lab1-src/test1-2.c.o"
+  "/home/hanwang/cs252/lab1-src/test1-3.c" "/home/hanwang/cs252/cmake-build-debug/CMakeFiles/cs252.dir/lab1-src/test1-3.c.o"
+  "/home/hanwang/cs252/lab1-src/test1-4.c" "/home/hanwang/cs252/cmake-build-debug/CMakeFiles/cs252.dir/lab1-src/test1-4.c.o"
+  "/home/hanwang/cs252/lab1-src/test1.c" "/home/hanwang/cs252/cmake-build-debug/CMakeFiles/cs252.dir/lab1-src/test1.c.o"
+  "/home/hanwang/cs252/lab1-src/test10.c" "/home/hanwang/cs252/cmake-build-debug/CMakeFiles/cs252.dir/lab1-src/test10.c.o"
+  "/home/hanwang/cs252/lab1-src/test2.c" "/home/hanwang/cs252/cmake-build-debug/CMakeFiles/cs252.dir/lab1-src/test2.c.o"
+  "/home/hanwang/cs252/lab1-src/test3.c" "/home/hanwang/cs252/cmake-build-debug/CMakeFiles/cs252.dir/lab1-src/test3.c.o"
+  "/home/hanwang/cs252/lab1-src/test4.c" "/home/hanwang/cs252/cmake-build-debug/CMakeFiles/cs252.dir/lab1-src/test4.c.o"
+  "/home/hanwang/cs252/lab1-src/test5.c" "/home/hanwang/cs252/cmake-build-debug/CMakeFiles/cs252.dir/lab1-src/test5.c.o"
+  "/home/hanwang/cs252/lab1-src/test6.c" "/home/hanwang/cs252/cmake-build-debug/CMakeFiles/cs252.dir/lab1-src/test6.c.o"
+  "/home/hanwang/cs252/lab1-src/test7.c" "/home/hanwang/cs252/cmake-build-debug/CMakeFiles/cs252.dir/lab1-src/test7.c.o"
+  "/home/hanwang/cs252/lab1-src/test8.c" "/home/hanwang/cs252/cmake-build-debug/CMakeFiles/cs252.dir/lab1-src/test8.c.o"
+  "/home/hanwang/cs252/lab1-src/test9.c" "/home/hanwang/cs252/cmake-build-debug/CMakeFiles/cs252.dir/lab1-src/test9.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
